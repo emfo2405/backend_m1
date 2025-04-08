dccc3362af5f6f5eec0a18f5c944d0e0b6917db4
emfo2405 <emfo2405@student.miun.se>
2025-04-08 16:08:28 +0200

    databas skapad

--- install.js
+++ install.js
@@ -1,1 +1,21 @@
 const sqlite3 = require("sqlite3").verbose();
+
+//Skapa en databas för projektet
+const db = new sqlite3.Database("./db/courses.db");
+
+//Skapa tabell i databas (id, kurskod, kursnamn, kursplan, progression)
+//Gör så att databas raderas för att därefter uppdateras
+db.serialize(() => {
+    db.run("DROP TABLE IF EXISTS courses;");
+
+    db.run(`
+        CREATE TABLE courses(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        coursecode TEXT NOT NULL,
+        coursename TEXT NOT NULL,
+        syllabus TEXT NOT NULL,
+        progression TEXT NOT NULL);
+        `);
+});
+
+db.close();
