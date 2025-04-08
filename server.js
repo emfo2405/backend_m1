ec630cc81bc40d0f3c73204788c9a98506016e2a
emfo2405 <emfo2405@student.miun.se>
2025-04-08 20:55:15 +0200

    tillägg att kunna skapa en kurs från data i formuläret

--- server.js
+++ server.js
@@ -14,7 +14,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Routing
 app.get("/", (req,res) => {
-    res.render("index");
+    res.render("index", {
+        error: ""
+    });
+});
+
+//Skapa ny kurs
+app.post("/", (req,res) => {
+    let coursecode = req.body.coursecode;
+    let coursename = req.body.coursename;
+    let syllabus = req.body.syllabus;
+    let progression = req.body.progression;
+    let error = "";
+
+    //Validera input
+    if(coursecode !== "" && coursename !== "" && syllabus !== "" && progression !== "") {
+
+    }else {
+        error = "Kontrollera att alla fält är korrekt ifyllda";
+    }
+
+    res.render("index", {
+        error: error
+    });
+
 });
 
 //Starta applikationen
